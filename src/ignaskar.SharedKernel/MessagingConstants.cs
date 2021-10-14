63c1dc59c395fabb74e1250c4f96601900f90e11
ignaskar <ignas.karpusenkovas@gmail.com>
2021-10-14 19:57:25 +0300

    Add MessagingConstants.cs

---
+++ src/ignaskar.SharedKernel/MessagingConstants.cs
@@ -0,0 +1,20 @@
+namespace ignaskar.SharedKernel
+{
+    public static class MessagingConstants
+    {
+        // can be overridden later with config values but serve as defaults otherwise.
+        public static class Credentials
+        {
+            public const string DEFAULT_USERNAME = "guest";
+            public const string DEFAULT_PASSWORD = "guest";
+        }
+        
+        // TODO: Exchanges & Queues once these are defined.
+
+        public static class NetworkConfig
+        {
+            public const int DEFAULT_PORT = 5672;
+            public const string DEFAULT_VIRTUAL_HOST = "/";
+        }
+    }
+}
